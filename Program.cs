22eba3eb0da9eb06dd5531e6b1f80bbb5528cb18
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-06-05 08:52:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment_5._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print the first n Natural numbers: ");
+            Console.WriteLine("----------------------------------\n");
+            Console.WriteLine("How many numbers to Print: ");
+
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            PrintNatural(1, count);
+            Console.WriteLine();
+        }
+
+        static int PrintNatural(int value,  int count)
+        {
+            if ( count < 1 )
+            {
+                return value;
+            }
+            count--;
+            Console.Write("{0}", value);
+            return PrintNatural(value + 1, count);
+        }
+    }
+}
